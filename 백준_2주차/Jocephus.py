a2f4501a4a517b6472e62e89bf033f402091f640
rhy2301 <gangri20040512@gmail.com>
2024-10-09 13:22:58 +0900

    Add files via upload

---
+++ 백준_2주차/Jocephus.py
@@ -0,0 +1,16 @@
+N, K = map(int, input().split())
+
+
+def josephus(N, K):
+    people = list(range(1,N+1))
+    result = []
+    index = 0
+
+    while people:
+        index = (index + K - 1)%len(people)
+        result.append(people.pop(index))
+
+    return result
+
+result = josephus(N,K)
+print("<"+", ".join(map(str, result))+">")
